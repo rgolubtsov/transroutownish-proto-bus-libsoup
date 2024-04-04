df904e1e3860c1facb9e1d91c69e8d42c0451ea6
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2024-04-04 18:50:30 +0300

    Add constants: common error messages, location of the sample routes data store, server port number left/right limits.

--- src/busd.h
+++ src/busd.h
@@ -28,17 +28,30 @@
 #define NEW_LINE   "\n"
 
 // Common error messages.
+#define ERR_PORT_VALID_MUST_BE_POSITIVE_INT "Valid server port must be " \
+    "a positive integer value, in the range 1024 .. 49151. " \
+    "The default value of 8080 will be used instead."
 #define ERR_SETTINGS_NOT_FOUND "Daemon settings file not found: %s"
+#define ERR_DATASTORE_NOT_FOUND "FATAL: Data store file not found. Quitting..."
 
 // Common notification messages.
 #define MSG_SERVER_STARTED "%s: Server started on port %u"
 #define MSG_SERVER_STOPPED "%s: Server stopped"
 
-/** The default server port number. */
-#define DEF_PORT 8080
-
 /** The path and filename of the daemon settings. */
 #define SETTINGS "./etc/settings.conf"
+
+/** The path and filename of the sample routes data store. */
+#define SAMPLE_ROUTES "./data/routes.txt"
+
+/** The minimum port number allowed. */
+#define MIN_PORT 1024
+
+/** The maximum port number allowed. */
+#define MAX_PORT 49151
+
+/** The default server port number. */
+#define DEF_PORT 8080
 
 // Daemon settings keys for the server port number.
 #define SERVER_GROUP "Server"
@@ -54,12 +67,19 @@
 #define PATH_DIR     "datastore.path.dir"
 #define FILENAME     "datastore.filename"
 
+// Retrieves the port number used to run the server, from daemon settings.
+unsigned short get_server_port(GKeyFile *);
+
+// Identifies whether debug logging is enabled by retrieving
+// the corresponding setting from daemon settings.
+gboolean is_debug_log_enabled(GKeyFile *);
+
+// Retrieves the path and filename of the routes data store
+// from daemon settings.
+gchar *get_routes_datastore(GKeyFile *);
+
 // Helper protos.
 GKeyFile *_get_settings();
-
-unsigned short get_server_port(GKeyFile *);
-gboolean is_debug_log_enabled(GKeyFile *);
-gchar *get_routes_datastore(GKeyFile *);
 
 #endif//BUSD_H
 
