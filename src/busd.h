c8782a1f828b0f3452639822ed181d88ffed7fd9
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2024-09-03 23:25:00 +0300

    Bump version number to 0.2.5.

--- src/busd.h
+++ src/busd.h
@@ -1,7 +1,7 @@
 /*
  * src/busd.h
  * ============================================================================
- * Urban bus routing microservice prototype (C port). Version 0.2.0
+ * Urban bus routing microservice prototype (C port). Version 0.2.5
  * ============================================================================
  * A daemon written in C (GNOME/libsoup), designed and intended to be run
  * as a microservice, implementing a simple urban bus routing prototype.
